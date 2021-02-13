40f80109b8144c572fdad5164fd0655455984f0d
zhichao <1522554132@qq.com>
2021-02-13 22:57:30 +0800

    包的作用域

---
+++ src/com/imo/Student.java
@@ -0,0 +1,9 @@
+package com.imo;
+
+public class Student {
+	String name;
+	public void sayHello() {
+		System.out.println("hello");
+	}
+
+}
---
+++ src/com/imo/Test.java
@@ -0,0 +1,12 @@
+package com.imo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Student student=new Student();
+		student.name="老二";
+		student.sayHello();
+
+	}
+
+}
